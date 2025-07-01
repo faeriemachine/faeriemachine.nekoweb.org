903d274915b8ee9de5198d15ea2bd4b5cb10d423
faeriemachine <thepallascat@gmail.com>
2025-07-01 09:10:46 -0400

    First Commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/js");
+
+  eleventyConfig.addCollection('posts', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
+  })
+
+    return {
+      passthroughFileCopy: true,
+      dir: {
+        input: "src",
+        output: "public",
+        includes: "_includes",
+      },
+      templateFormats: ['md', 'njk', 'html'],
+      markdownTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
+    };
+  };
