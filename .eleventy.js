63563e5b90cf7c88633667f0ffc7dd333ac8741d
faeriemachine <thepallascat@gmail.com>
2025-07-02 14:12:29 -0400

    Delete update njks files, replace with HTML

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/md");
   eleventyConfig.addFilter("date", require("./src/filters/date.js"));
 
   eleventyConfig.addCollection('posts', function(collectionApi) {
     return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
+
+  });
+
+  eleventyConfig.addCollection('updates', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/updates/**/*.md');
 
   });
 
